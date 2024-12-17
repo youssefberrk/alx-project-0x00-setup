d083af7365b45af246dbd6ba5947fc57a8804441
youssef <yberrakouan.lol@gmail.com>
2024-12-17 11:45:17 +0100

    Basic Routing

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-extralight">Landing Page</h1>
+    </div>
+  );
+};
+
+export default Landing;
