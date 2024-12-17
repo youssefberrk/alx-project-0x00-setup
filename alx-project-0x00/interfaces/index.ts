ec5268a1af235bfa697be51b06b8c29e05c7fd7a
youssef <yberrakouan.lol@gmail.com>
2024-12-17 11:49:32 +0100

    Basic Routing

---
+++ alx-project-0x00/interfaces/index.ts
@@ -0,0 +1,16 @@
+// interfaces/index.ts
+
+// Example interface for a user
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+  }
+  
+  // Example interface for props for a component
+  export interface LandingProps {
+    title: string;
+  }
+  
+  // Example for a functional component with typed props
+  export type FunctionalComponentWithProps = React.FC<LandingProps>;  
