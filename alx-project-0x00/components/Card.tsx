075b34c2f7bccee27057d54e3e4644e7720cd418
youssef <yberrakouan.lol@gmail.com>
2024-12-17 12:17:38 +0100

    Using components

--- alx-project-0x00/components/Card.tsx
+++ alx-project-0x00/components/Card.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
-import HOUSE_IMAGE from "@/public/assets/house.png"
-import STAR_IMAGE from "@/public/assets/star.png"
+import HOUSE_IMAGE from "@/public/assets/house.png";
+import STAR_IMAGE from "@/public/assets/star.png";
 import React from "react"
 import Pill from "./Pill"
 
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Card from "@/components/Card";
 
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
+      <Card />
     </div>
   );
 };
